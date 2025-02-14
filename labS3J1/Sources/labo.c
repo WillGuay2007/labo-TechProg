763c92ecbe54602d6d75574ab139f80239aca9f2
William Guay <2430063@collegebart.ca>
2025-02-14 13:56:45 -0500

    Voici mes labos de la semaine 2

---
+++ labS3J1/Sources/labo.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "labo.h"
+
+void* allocate(size_t size);
+
+
+void insert(Node* currNode, void* newData) {
+
+}
+
+/*
+* Creer un noeud et l'ajouter apres le noeud head. Si le noeud head est vide(data) lui donner la nouvelle valeur passer.
+*/
+void insertHead(Node* head, void* newData) {
+
+}
+
+/*
+* Supprimer le noeud ayant la valeur de pointeur de data i.e. node->data == rmData. Par simplicite, pour supprimer faite un memset(node,0,sizeof(Node));
+* Retourner le noeud juste avant celui supprimer
+*/
+Node* removeByData(Node* head, void* rmData) {
+	
+}
+
+/*
+* Supprimer le noeud ayant le nom passer par name.Par simplicite, pour supprimer faite un memset(node,0,sizeof(Node));
+* Retourner le noeud juste avant celui supprimer
+*/
+Node* removeByName(Node* head, char* name) {
+
+}
+
+/*
+* Trier par l'age. Utiliser l'algorithme que vous connaissez le mieux.
+*/
+void sort(Node* head) {
+
+}
+
+int main() {
+
+}
