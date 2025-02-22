a4fb6d51b13acd9501a7949fd189ebb63bdd6bd9
WillGuay2007 <2430063@bart.ca>
2025-02-21 20:05:04 -0500

    Labos semaine 3

--- labS3J2/Sources/labo.c
+++ labS3J2/Sources/labo.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "labo.h"
 
-void insert(Node* currNode, void* newData)
-{
+void insert(Node* currNode, void* newData) {
+	//Je sais pas pourquoi ca marche pas
+	if (currNode == NULL) {
+		return;
+	}
+
 	Node* n = allocate(sizeof(Node));
 	n->data = newData;
 	n->prev = NULL;
@@ -16,10 +20,6 @@ void insert(Node* currNode, void* newData)
 	n->prev = currNode;
 }
 
-/*
-* Creer un noeud et l'ajouter apres le noeud de fin. Si le noeud head est vide(data) lui donner la nouvelle valeur passer.
-*/
-
 void insertTail(Node* head, void* newData)
 {
 	Node* CurrNode;
@@ -32,27 +32,44 @@ void insertTail(Node* head, void* newData)
 		Node* newNode = allocate(sizeof(Node));
 		newNode->data = newData;
 		CurrNode = head;
-		while (CurrNode->next != head)
+		while (CurrNode->next != NULL)
 		{
 			CurrNode = CurrNode->next;
 		}
-		insert(CurrNode, newNode);
-		newNode->next = head;
-		head->prev = newNode;
+		insert(CurrNode, newData);
 	}
 }
 
 void insertHead(Node* head, void* newData)
 {
+	if (head->data == NULL) {
+		head->data = newData;
+		head->next = head;
+		head->prev = head;
+	}
+	else {
+		Node* newNode = allocate(sizeof(Node));
+		newNode->data = newData;
 
+		newNode->next = head;
+		head->prev = newNode;
+
+
+	}
 }
 
 Node* removeNode(Node* currNode)
 {
-	return NULL;
+	if (currNode == NULL) return NULL;
+	currNode->prev->next = currNode->next;
+	currNode->next->prev = currNode->prev;
+	currNode->prev = NULL;
+	currNode->next = NULL;
+
+	return currNode;
 }
 
 void alphabetise(Node* head, char* names[])
 {
-
+	// je suis censé sort le data des nodes ou sort le tableau de strings?
 }
